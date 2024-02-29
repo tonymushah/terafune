be333df1b2aa40a59036ce84e44734e993d57f41
tonymushah <tonyramanantsalama@gmail.com>
2024-02-29 15:50:24 +0300

    Added export for `t-invoke-button`

--- packages/terafune/src/componnents/index.ts
+++ packages/terafune/src/componnents/index.ts
@@ -1,1 +1,2 @@
-export { default as InvokeElement } from "./invoke"; 
+export { default as InvokeElement } from './invoke'
+export { default as InvokeButtonElement } from './invoke-button'
--- packages/terafune/src/index.ts
+++ packages/terafune/src/index.ts
@@ -1,7 +1,10 @@
-import { InvokeElement } from "./componnents";
+import { InvokeButtonElement, InvokeElement } from './componnents'
 
-export function register (): void {
-  customElements.define("t-invoke", InvokeElement);
+export function register(): void {
+  customElements.define('t-invoke', InvokeElement)
+  customElements.define('t-invoke-button', InvokeButtonElement, {
+    extends: 'button'
+  })
 }
 
-export * from "./componnents";
+export * from './componnents'
